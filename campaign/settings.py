80b940d07fccbcd7e3be11c9bfebd8d9e018daba
Mau-exporters <mauexporters@gamail.com>
2026-06-26 18:35:32 +0300

    Add cloudinary media storage

--- campaign/settings.py
+++ campaign/settings.py
@@ -3,6 +3,7 @@ Django settings for campaign project.
 """
 
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,6 +119,10 @@ LOGIN_URL = '/admin/login/'
 
 # ================= DEFAULT AUTO FIELD =================
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEFAULT_FILE_STORAGE = (
-    'cloudinary_storage.storage.MediaCloudinaryStorage'
+import cloudinary
+
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET")
 )
