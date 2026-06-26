589d1ce5e98e182be6e8490ad999b682bcd5fe7a
Mau-exporters <mauexporters@gamail.com>
2026-06-26 20:36:34 +0300

    Create Render admin user

--- campaign/settings.py
+++ campaign/settings.py
@@ -69,16 +69,11 @@ WSGI_APPLICATION = 'campaign.wsgi.application'
 
 
 # ================= DATABASE =================
-import dj_database_url
-
-
 DATABASES = {
-
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
